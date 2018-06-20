fd6bc60c7a186e5ec5195f6369264150e264711e
Sultan Alsawaf <sultanxda@gmail.com>
2018-06-20 09:44:16 +0000

    markw: Enable cpuidle during boot
    * This is only used to keep cpuidle disabled during boot, which is 
    unnecessary and doesn't really improve boot times, but does incur a high 
    power cost

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,10 +36,10 @@ TARGET_USES_64_BIT_BINDER := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/xiaomi/markw
 TARGET_KERNEL_CONFIG := markw_defconfig
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 TARGET_KERNEL_VERSION := 3.18
 
@@ -48,7 +48,6 @@ BOARD_KERNEL_CMDLINE := \
     androidboot.bootdevice=7824900.sdhci \
     earlycon=msm_hsl_uart,0x78af000 \
     androidboot.selinux=permissive \
-    lpm_levels.sleep_disabled=1 \
     androidboot.hardware=qcom \
     msm_rtb.filter=0x237 \
     ehci-hcd.park=3
