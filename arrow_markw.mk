4cd1840c3fffd59f2dee58efc1e55d511a53c9ac
DmFrPro <dmfr2021y@gmail.com>
2020-01-12 14:26:52 +0400

    markw: ArrowOS bring-up

--- arrow_markw.mk
+++ arrow_markw.mk
@@ -17,10 +17,12 @@
 # Inherit from those products. Most specific first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from markw device
 $(call inherit-product, device/xiaomi/markw/device.mk)
+
+# Inherit ArrowOS stuff
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 BOARD_VENDOR := Xiaomi
@@ -29,7 +31,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := markw
 PRODUCT_MODEL := Redmi 4 Prime
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_markw
+PRODUCT_NAME := arrow_markw
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
